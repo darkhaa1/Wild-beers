2b5147e99047c14caa65eb4d611822c091d09c51
Lise PERARD <liseprd@hotmail.fr>
2024-11-14 10:08:08 +0100

    creation du bouton validé

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -85,61 +85,66 @@
 //   const [filteredBreweries, setFilteredBreweries] = useState<Beer[]>(breweries);
 //   const [filter, setFilter] = useState(false);
 
-//   const handleCountryChange = (country: string) => {
-//     const newSelectedCountries = selectedCountries.includes(country)
-//       ? selectedCountries.filter((c) => c !== country)
-//       : [...selectedCountries, country];
-//     setSelectedCountries(newSelectedCountries);
-//     setSelectedProvinces([]);
-//     setSelectedCities([]);
-//     filterBreweries(newSelectedCountries, [], []);
+//   const [tempCountries, setTempCountries] = useState<string[]>([]);
+//   const [tempProvinces, setTempProvinces] = useState<string[]>([]);
+//   const [tempCities, setTempCities] = useState<string[]>([]);
+
+//   const handleTempCountryChange = (country: string) => {
+//     const newSelectedCountries = tempCountries.includes(country)
+//       ? tempCountries.filter((c) => c !== country)
+//       : [...tempCountries, country];
+//     setTempCountries(newSelectedCountries);
 //   };
 
-//   const handleProvincesChange = (province: string) => {
-//     const newSelectedProvinces = selectedProvinces.includes(province)
-//       ? selectedProvinces.filter((r) => r !== province)
-//       : [...selectedProvinces, province];
-//     setSelectedProvinces(newSelectedProvinces);
-//     setSelectedCities([]);
-//     filterBreweries(selectedCountries, newSelectedProvinces, []);
+//   const handleTempProvincesChange = (province: string) => {
+//     const newTempProvinces = tempProvinces.includes(province)
+//       ? tempProvinces.filter((r) => r !== province)
+//       : [...tempProvinces, province];
+//     setTempProvinces(newTempProvinces);
 //   };
 
-//   const handleCitiesChange = (city: string) => {
-//     const newSelectedCities = selectedCities.includes(city)
-//       ? selectedCities.filter((c) => c !== city)
-//       : [...selectedCities, city];
-//     setSelectedCities(newSelectedCities);
-//     filterBreweries(selectedCountries, selectedProvinces, newSelectedCities);
+//   const handleTempCitiesChange = (city: string) => {
+//     const newTempCities = tempCities.includes(city)
+//       ? tempCities.filter((c) => c !== city)
+//       : [...tempCities, city];
+//     setTempCities(newTempCities);
+//   };
+
+//   const handleApplyFilters = () => {
+//     setSelectedCountries(tempCountries);
+//     setSelectedProvinces(tempProvinces);
+//     setSelectedCities(tempCities);
+//     filterBreweries(tempCountries, tempProvinces, tempCities);
 //   };
 
 //   const provinces = Array.from(
 //     new Set(
 //       breweries
-//         .filter((brewery) => selectedCountries.includes(brewery.country))
-//         .map((brewery) => brewery.state_province)
-//     )
+//         .filter((brewery) => tempCountries.includes(brewery.country))
+//         .map((brewery) => brewery.state_province),
+//     ),
 //   );
 
 //   const cities = Array.from(
 //     new Set(
 //       breweries
-//         .filter((brewery) => selectedProvinces.includes(brewery.state_province))
-//         .map((brewery) => brewery.city)
-//     )
+//         .filter((brewery) => tempProvinces.includes(brewery.state_province))
+//         .map((brewery) => brewery.city),
+//     ),
 //   );
 
 //   const filterBreweries = (
 //     countries: string[],
 //     provinces: string[],
-//     cities: string[]
+//     cities: string[],
 //   ) => {
 //     setFilteredBreweries(
 //       breweries.filter(
 //         (brewery) =>
 //           (!countries.length || countries.includes(brewery.country)) &&
 //           (!provinces.length || provinces.includes(brewery.state_province)) &&
-//           (!cities.length || cities.includes(brewery.city))
-//       )
+//           (!cities.length || cities.includes(brewery.city)),
+//       ),
 //     );
 //   };
 
@@ -158,8 +163,8 @@
 //                 <input
 //                   type="checkbox"
 //                   id={country}
-//                   checked={selectedCountries.includes(country)}
-//                   onChange={() => handleCountryChange(country)}
+//                   checked={tempCountries.includes(country)}
+//                   onChange={() => handleTempCountryChange(country)}
 //                 />
 //                 <label htmlFor={country}>{country}</label>
 //               </div>
@@ -167,14 +172,14 @@
 
 //             {provinces.length > 0 && (
 //               <>
-//                 <h2>Select Regions</h2>
+//                 <h2>Régions/States</h2>
 //                 {provinces.map((province) => (
 //                   <div key={province}>
 //                     <input
 //                       type="checkbox"
 //                       id={province}
-//                       checked={selectedProvinces.includes(province)}
-//                       onChange={() => handleProvincesChange(province)}
+//                       checked={tempProvinces.includes(province)}
+//                       onChange={() => handleTempProvincesChange(province)}
 //                     />
 //                     <label htmlFor={province}>{province}</label>
 //                   </div>
@@ -184,23 +189,25 @@
 
 //             {cities.length > 0 && (
 //               <>
-//                 <h2>Select Cities</h2>
+//                 <h2>Villes</h2>
 //                 {cities.map((city) => (
 //                   <div key={city}>
 //                     <input
 //                       type="checkbox"
 //                       id={city}
-//                       checked={selectedCities.includes(city)}
-//                       onChange={() => handleCitiesChange(city)}
+//                       checked={tempCities.includes(city)}
+//                       onChange={() => handleTempCitiesChange(city)}
 //                     />
 //                     <label htmlFor={city}>{city}</label>
 //                   </div>
 //                 ))}
 //               </>
 //             )}
+//             <button type="button" onClick={handleApplyFilters}>
+//               Valider
+//             </button>
 //           </div>
 //         )}
-
 //         <ul>
 //           {filteredBreweries.map((brewery) => (
 //             <li key={brewery.id}>
