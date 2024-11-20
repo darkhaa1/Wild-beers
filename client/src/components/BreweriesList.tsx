d822df7c6fbf2a17d26e789731accccf21cacf17
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-11-20 14:52:53 +0100

    creation css mobile page home

--- client/src/components/BreweriesList.tsx
+++ client/src/components/BreweriesList.tsx
@@ -20,36 +20,39 @@ const BreweriesList = () => {
   const [breweries, setBreweries] = useState<Brewery[]>([]);
 
   useEffect(() => {
-    fetch("https://api.openbrewerydb.org/v1/breweries?per_page=200")
+    fetch("https://api.openbrewerydb.org/v1/breweries?per_page=4")
       .then((response) => response.json())
       .then((data) => setBreweries(data));
   }, []);
 
   return (
     <div>
-      <ul>
-        {breweries.map((brewery) => (
-          <figure className="card" key={brewery.id}>
-            <h3 className="nameBrasserie">{brewery.name}</h3>
-            <ul className="cardList">
-              <li>Type de brassage: {brewery.brewery_type}</li>
-              <li>
-                Adresse :{brewery.address} {brewery.postal_code}{" "}
-                <li>Ville :{brewery.city}</li>
-                <li>Région :{brewery.state}</li>
-                <li>Pays : {brewery.country}</li>
-              </li>
-              <li>
-                Site internet :{" "}
-                <a href={brewery.website_url}>visiter le site </a>
-              </li>
-            </ul>
-            <button type="button" onClick={() => toggleFavorite(brewery.id)}>
-              {favorites.includes(brewery.id) ? "❤️" : "🖤"}
-            </button>
-          </figure>
-        ))}
-      </ul>
+      {breweries.map((brewery) => (
+        <figure className="card" key={brewery.id}>
+          <h3 className="nameBrasserie">{brewery.name}</h3>
+          <ul className="cardList">
+            <li>Type de brassage: {brewery.brewery_type}</li>
+            <li>
+              Adresse :{brewery.address} {brewery.postal_code}{" "}
+              <li>Ville :{brewery.city}</li>
+              <li>Région :{brewery.state}</li>
+              <li>Pays : {brewery.country}</li>
+            </li>
+            <li>
+              <a href={brewery.website_url} className="url">
+                visiter le site{" "}
+              </a>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="coeurs"
+            onClick={() => toggleFavorite(brewery.id)}
+          >
+            {favorites.includes(brewery.id) ? "❤️" : "🖤"}
+          </button>
+        </figure>
+      ))}
     </div>
   );
 };
